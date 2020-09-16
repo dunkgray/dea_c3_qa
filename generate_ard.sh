5c02ec7601733d0393a01718073637a4b74268c9
Duncan Gray <duncan.gray@ga.gov.au>
2020-09-16 15:29:49 +1000

    set up for test generation

--- generate_ard.sh
+++ generate_ard.sh
@@ -2,7 +2,9 @@
 module purge
 module load pbs
 
+OUTPUTDIR=$(date +"%Y-%m-%d-%H-%M-%S")
+TESTDIR=$PWD/../ard_test/$OUTPUTDIR
 
 source prod-wagl.env
 
-ard_pbs --level1-list scenes_to_ARD_process.txt --workdir ../scratch --pkgdir ../ard_ref /g/data/xu18/ga --env prod-wagl.env --project u46 --walltime 02:30:00  --nodes 1 --logdir ../logdir
+ard_pbs --level1-list scenes_to_ARD_process.txt --workdir $TESTDIR --pkgdir $TESTDIR --env $PWD/prod-wagl.env --project u46 --walltime 02:30:00  --nodes 1 --logdir  $TESTDIR
